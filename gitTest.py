c341fa2512cebeedd65ac1fc8b123ca65e347d6f
Hang Chen <hangchen@Hangs-Air.lan>
2023-06-15 22:15:23 -0400

    retrieve

---
+++ gitTest.py
@@ -0,0 +1,21 @@
+# worker A's code
+workerA = 0
+for i in range(3):
+    workerA += 1
+print("This is worker A's code: ", workerA)
+
+
+# worker B's code
+workerB = 0
+for i in range(3):
+    workerB -= 1
+print("This is worker B's code: ", workerB)
+
+
+# Notes for git: 
+# 1, git rm/mv: directly remove and rename files in stage/index, skipping add step.
+# 2, git status -s: brief version of status, showing local and staged status for files.
+# 3, unstage/restore files from index/stage (undo add command): git restore --staged fileName.
+# 4, reset LOCAL changes to previous stage: git restore fileName.
+# 5, retrieve commited and deleted files by undoing the commit: git restore --source=HEAD~1 fileName
+# 6, 
